4086091a0391be89a90b25c152a8ffd1d8a88537
kchalasa <kantic@hotmail.com>
2014-04-23 21:13:23 -0400

    Programming Assignment 2 Commit with comments Apr 23 2014

--- cachematrix.R
+++ cachematrix.R
@@ -8,14 +8,19 @@
 
 makeCacheMatrix <- function(x = matrix()) {
      m <- NULL
-     set <- function(y) {
-          x <<- y
-          m <<- NULL
-     }
-     get <- function() x
-     setInverse <- function(solve) m <<- solve
-     getInverse <- function() m
-     list(set = set, get = get,
+     set <- function(y) {              
+                  x <<- y           ## Store Original Matrix
+                  m <<- NULL        ## Clear Matrix Inverse
+                 }
+
+     get <- function() x            ## Return Original Matrix
+
+     setInverse <- function(solve) { 
+     		      m <<- solve   ## Store Matrix Inverse
+	           }
+     getInverse <- function() m     ## Return Matrix Inverse
+
+     list(set = set, get = get,     ## Return list of all the above functions
 	 setInverse = setInverse,
  	 getInverse = getInverse)
 }
@@ -24,14 +29,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function takes our customized list (makeCacheMatrix) as input parameter
 ## Uses functions in this customized list to return Inverse of a Matrix
 ## It uses "solve" R-function to take a square matrix and return its Inverse.
+
 cacheSolve <- function(x, ...) {
-    m <- x$getInverse()
-    if(!is.null(m)) {
-         message("getting cached data")
-         return(m)
-    }
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setInverse(m)
-    m  ## Return a matrix that is the inverse of 'x'
+     m <- x$getInverse()            ## Func. to get matrix Inverse from Cache.
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)                 ## Found in CACHE so return matrix inverse
+     }
+     data <- x$get()                ## Invoke function to get orig. matrix 'x'
+     m <- solve(data, ...)          ## R function to compute matrix inverse
+     x$setInverse(m)                ## Invoke function for matrix inverse
+     m                              ## Return matrix that is the inverse of 'x'
 }
